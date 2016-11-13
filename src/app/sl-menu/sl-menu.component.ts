294b3a0419dc56a6a865272dca4e6e6a2001e2e1
Will Howell <will.s.howell@gmail.com>
2016-11-12 22:09:00 -0500

    Use scss instead of sass and style more of the menu content

--- src/app/sl-menu/sl-menu.component.ts
+++ src/app/sl-menu/sl-menu.component.ts
@@ -3,11 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'sl-menu',
   templateUrl: './sl-menu.component.html',
-  styleUrls: ['./sl-menu.component.sass']
+  styleUrls: ['./sl-menu.component.scss']
 })
 export class SlMenuComponent implements OnInit {
 
-  constructor() { }
+  authenticated: boolean;
+
+  constructor() { 
+    this.authenticated = false;
+  }
 
   ngOnInit() {
   }
